191459579fb499f4e2338dabb4a59be9f4e24ccf
Pablo Sun <pablo.sun@mediatek.com>
2015-07-17 12:33:12 +0800

    Fix avrdude conf and path for IDE 1.6.4

--- mtk/gen_repo.bat
+++ mtk/gen_repo.bat
@@ -4,8 +4,8 @@ set version=0.1.1
 set setupname=mediatek_linkit_smart_7688_
 set sdk_zip_name=%setupname%%version%.zip
 
-del .\out\%setupname%%version%.zip /q
-mkdir out
+IF EXIST ".\out\%setupname%%version%.zip" del /F /Q ".\out\%setupname%%version%.zip"
+IF NOT EXIST ".\out" mkdir out
 
 7z.exe a ".\out\%sdk_zip_name%" ".\avr"
 
